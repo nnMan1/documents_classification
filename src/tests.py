d9ce9dd1be250e69c01d1f82b11637594aed0f0b
velibor <dosljakvelibor@gmail.com>
2020-05-22 11:39:03 +0200

    Fixes problem with finding outliers made by copy paste

--- src/tests.py
+++ src/tests.py
@@ -13,41 +13,18 @@ from sklearn.naive_bayes import MultinomialNB
 #def check_novelty(model, data):
    
     
-def find_outliers(distance_matrix):   
-
-    classification = classifier.get_classification()
-
+def find_outliers(distance_matrix):    
     classifier = LocalOutlierFactor(n_neighbors=8, algorithm='brute', metric='precomputed', contamination=0.07, n_jobs=-1)
     ans = classifier.fit_predict(distance_matrix)
-   
-    return [i for i in range(len(ans)) if ans[i]==-1]
+    return ans
 
 data = data_loader.load_documents()
-
 distance_matrix = Distance([d.text  for d in data['train'][:1000]], min_similarity=0.13).distances
-tmp = find_outliers(distance_matrix)
-for i in range(len(tmp)):
+ans = find_outliers(distance_matrix)
+
+for i in range(len(ans)):
+   if ans[i]==-1:
         print(i, data['train'][i].category)
-#distance_matrix = joblib.load('./sgd_data005.pkl').distances
-
-#classifier = AffinityPropagation(damping=0.5, max_iter=200, convergence_iter=15, copy=True, preference=None, affinity='precomputed')
-#classifier = DBSCAN(eps=0.75, min_samples=find_outliers(distance_matrix.distances)8, metric='precomputed', algorithm='auto', leaf_size=30, n_jobs=-1)
-
-#classes = classifier.fit_predict(distance_matrix.distances)
-
-#find_outliers(distance_matrix.distances)
-
-#tmp = find_outliers(distance_matrix)
-
-
-
-#for outlier in outliers:
-   # print(outlier, data['train_classes'][outlier])
-
-
-#check_novelty('./sgd_classifier.pkl', './sdg_vectorizer.pkl', './sgd_transformer.pkl', './sgd_traindata.pkl', '../novelties/data/normal_data')
-
-
 #print(ans)
 
 #ans = classifier.predict(Distance(data['test']).distances)
