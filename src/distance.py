cc4076e59caebf0787860549de802b25ea82dfc7
velibor <dosljakvelibor@gmail.com>
2020-05-23 12:32:03 +0200

    Impelemented optimization for calculating distance matrix

--- src/distance.py
+++ src/distance.py
@@ -12,47 +12,139 @@ class Distance:
     length_based_filtering_applied = False
 
     def __init__(self, data, min_similarity = 0.2):
-
         self.min_similarity = min_similarity
 
         self.distances = np.ones((len(data), len(data)))
         vectorizer = CountVectorizer(decode_error = 'ignore')
 
         tmp = vectorizer.fit_transform(data)
-        self.shingles = [set(sparse.nonzero()[1]) for sparse in tmp]
+        self.shingles = [sorted(sparse.nonzero()[1]) for sparse in tmp]
 
         universal_set = vectorizer.get_feature_names()
         self.universal_set = { universal_set[i] : i  for i in range(0, len(universal_set) ) }
-        
-        self.indexes = range(len(self.shingles)) #svi elementi su poredjani kako su usli
-        self.__apply_length_based_filtering()
-        self.__calculate_distance_matrix()
-    
-    def __apply_length_based_filtering(self):
-        
-        lengths = []
+
+        self.__calculate_token_frequency()
 
         for i in range(len(self.shingles)):
-            lengths.append(len(self.shingles[i]))
+            self.shingles[i] = sorted(self.shingles[i], key= self.token_frequency.__getitem__)
         
-        self.indexes = sorted(self.indexes, key= lengths.__getitem__)
+        self.__hash_data()
+
+    def __calculate_token_frequency(self):
+
+        token_frequency= [0]*len(self.universal_set)
         
+        for shingle in self.shingles:
+            for token in shingle:
+                token_frequency[token]+=1
+        
+        self.token_frequency = token_frequency
+            
+    def __hash_data(self):
+        hash_bucets = {}
+
+        for k in range(len(self.shingles)):
+            shingle = self.shingles[k]
+            l = int((1-self.min_similarity)*len(shingle)+1)
+            for i in range(min(len(shingle),l)):
+                if not shingle[i] in hash_bucets.keys():
+                    hash_bucets[shingle[i]]={}
+                
+                if not i in hash_bucets[shingle[i]].keys():
+                    hash_bucets[shingle[i]][i]={}
+
+                if not (len(shingle)-i-1) in hash_bucets[shingle[i]][i].keys():
+                    hash_bucets[shingle[i]][i][len(shingle)-i-1] = set()
+                
+                hash_bucets[shingle[i]][i][len(shingle)-i-1].add(k)
+
+        self.hash_bucets = hash_bucets
+
+    def __find_possible_documents(self, a, i, p):
+        possible = set()
+        l=p+i+1
+
+        if not a in self.hash_bucets.keys():
+            return possible
+        
+        j_values = [j for j in self.hash_bucets[a].keys() if j<=(p+1)/self.min_similarity - l]
+
+        for j in j_values:
+            q_values = [q for q in self.hash_bucets[a][j].keys() if (min(q,p)+1)/(max(p,q)+max(i,j)+1)]
+            for q in q_values:
+                possible = possible.union(self.hash_bucets[a][j][q])
+        
+        return possible
+
     def __jacard_distance(self, a, b):
         intersection = set(a).intersection(set(b))
         ans = len(intersection) / (len(a) + len(b) - len(intersection))
         return ans
 
-    def __calculate_distance_matrix(self):
-         for i in range(len(self.shingles)):
+    def calculate_distance_matrix_opptimized(self):
+
+        for i in range(len(self.shingles)):
+            a = self.shingles[i]
+            possible = set()
+            for k in range(min(len(a), int((1-self.min_similarity)*len(a)+1))):
+                possible = possible.union(self.__find_possible_documents(a[k], k, len(a)-k-1))
+            
+            for j in possible:
+                b = self.shingles[j]
+                similarity = self.__jacard_distance(a,b)
+                if similarity >= self.min_similarity:
+                    self.distances[i,j]=0
+        
+        return self.distances
+
+    def calculate_distance_for_optimized(self, data):
+        vectorizer = CountVectorizer(decode_error = 'ignore')
+        tmp = vectorizer.fit_transform(data)
+
+        universal_set = vectorizer.get_feature_names()
+        cnt = len(self.universal_set)
+
+        for key in universal_set:
+            if not key in self.universal_set.keys():
+                self.universal_set[key] = cnt
+                cnt = cnt+1
+                self.token_frequency.append(1)
+            
+        shingles = []
+
+        for shingle in tmp:
+            shingle = shingle.nonzero()[1]
+            shingle = [self.universal_set[universal_set[i]] for i in shingle]
+            shingles.append(sorted(shingle, key= self.token_frequency.__getitem__))
+        
+        ans = np.ones((len(shingles), len(self.shingles)))
+
+        for i in range(len(shingles)):
+            a = shingles[i]
+            possible = set()
+            for k in range(min(len(a), int((1-self.min_similarity)*len(a)+1))):
+                possible = possible.union(self.__find_possible_documents(a[k], k, len(a)-k-1))
+            
+            for j in possible:
+                b = self.shingles[j]
+                similarity = self.__jacard_distance(a,b)
+                if similarity >= self.min_similarity:
+                    ans[i, j] = 0        
+        
+        return ans
+      
+    def calculate_distance_matrix(self):
+        for i in range(len(self.shingles)):
             self.distances[i,i] = 0
             for j in range(i,len(self.shingles)):
-                #similaruity = self.__jacard_distance(self.indexes[i], self.indexes[j])
                 similaruity = self.__jacard_distance(self.shingles[i],self.shingles[j])               
                 if(similaruity >= self.min_similarity):
                     self.distances[i, j] = 0
                     self.distances[j, i] = 0
+        
+        return self.distances
 
-    def calcilate_distance_for(self, data):
+    def calculate_distance_for(self, data):
         vectorizer = CountVectorizer(decode_error = 'ignore')
         tmp = vectorizer.fit_transform(data)
 
@@ -79,22 +171,20 @@ class Distance:
                     ans[j, i] = 0
         
         return ans
-                
-                
-
 
 if __name__=='__main__':
 
 
-    distance = Distance(['marko kraljevic', 'musa kesedzija', 'marko kraljevic i musa kesedzija'])
+    distance = Distance(['marko kraljevic', 'musa kesedzija', 'marko kraljevic i musa kesedzija', 'marko'])
     print(distance.distances)
 
-    print(distance.calcilate_distance_for(['marko markovic']))
+    print(distance.calculate_distance_for_optimized(['marko markovic']))
+    print(distance.calculate_distance_for(['marko markovic']))
 
 
 
     #data = data_loader.load_documents()['train']
-    #distance = Distance([d.text  for d in data[:500]])
+    #distance = Distance([d.text  for d in data])
     #joblib.dump(distance, './sgd_data005.pkl', compress=9)
     #distance = joblib.load('./sgd_data.pkl')
     #joblib.dump(distance, './sgd_data0.1.pkl', compress=9)
--- src/tests.py
+++ src/tests.py
@@ -1,5 +1,4 @@
 import data_loader
-from clasifier import Classifier
 import preload
 from distance import Distance
 import joblib
@@ -17,19 +16,21 @@ def find_outliers(distance_matrix):
     return classifier.predict(distance_matrix), classifier
 
 data = data_loader.load_documents()
-distance_matrix = Distance([d.text  for d in data['train'][:2000]], min_similarity=0.13)
-ans, classifier = find_outliers(distance_matrix.distances)
 
-#distance_matrix = Distance([d.text  for d in data['train'][:1000]].append(data['test'][11]), min_similarity=0.13).distances
+#distance = Distance([d.text  for d in data['train'][:2000]], min_similarity=0.13)
+distance = Distance([d.text  for d in data['train']], min_similarity=0.13)
+
+distance_matrix = distance.calculate_distance_matrix_opptimized()
+ans, classifier = find_outliers(distance_matrix)
 
 print("Outliers are")
 
 for i in range(len(ans)):
    #if ans[i]==-1 and (data['train'][i].category in [['earn'], ['acq'], ['money-fx'], ['trade']]):
    if ans[i]==-1:
-        print(i, data['train'][i].category)
+        print(i, data['test'][i].name, data['train'][i].category)
 
-distance_matrix = distance_matrix.calcilate_distance_for([d.text  for d in data['test'][:500]])
+distance_matrix = distance.calculate_distance_for_optimized([d.text  for d in data['test'][:500]])
 
 print("Novelties are")
 ans = find_novelties(classifier, distance_matrix)
@@ -37,4 +38,4 @@ ans = find_novelties(classifier, distance_matrix)
 for i in range(len(ans)):
    #if ans[i]==-1 and (data['train'][i].category in [['earn'], ['acq'], ['money-fx'], ['trade']]):
    if ans[i]==-1:
-        print(i, data['test'][i].category)
+        print(i, data['test'][i].name, data['test'][i].category)
